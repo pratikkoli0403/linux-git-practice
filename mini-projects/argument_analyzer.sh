697aa646de64ca29fb5d18069a850c65af731f9a
Pratik Koli <pratikkoli0403@gmail.com>
2025-12-26 07:54:43 +0000

    Refactor argument analyzer using finctuons

--- mini-projects/argument_analyzer.sh
+++ mini-projects/argument_analyzer.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
-if [ $# -eq 0 ];
-then
-echo "atleast give one argument"
+analyze_arg(){
+arg=$1
+length=${#arg}
+
+if [ $length -ge 5 ]; then
+echo "$arg length $length valid"
+else
+echo "$arg length $length invalid"
+fi
+}
+
+main(){
+if [ $# -eq 0 ]; then
+echo "Give at least one argument"
 exit 1
 fi
-count=0
 for arg in "$@"
 do
-((count++))
-length=${#arg}
-if [ $length -ge 5 ];
-then
-status="valid"
-else
-status="invalid"
-fi
-echo "$arg length $length $status"
+analyze_arg "$arg"
 done
-echo "Total argument:$count"
+echo "Total argument:$#"
+}
+main "$@"
