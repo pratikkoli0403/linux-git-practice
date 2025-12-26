27ff95bead90fa5b0f1a71c74f0ca1494bd5ec55
Pratik Koli <pratikkoli0403@gmail.com>
2025-12-26 08:26:02 +0000

    Add argument analyzer with function and exit codes

--- mini-projects/argument_analyzer.sh
+++ mini-projects/argument_analyzer.sh
@@ -16,10 +16,13 @@ if [ $# -eq 0 ]; then
 echo "Give at least one argument"
 exit 1
 fi
+invalid_found=0
+
 for arg in "$@"
 do
-analyze_arg "$arg"
+analyze_arg "$arg" || invalid_found=1
 done
 echo "Total argument:$#"
+[ $invalid_found -eq 1 ] && exit 2 ||exit 0
 }
 main "$@"
